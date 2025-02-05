d34de5d46a7cc9691a43bf45a970ec9633f2e79f
YirehEum97 <yreum97@daou.co.kr>
2025-02-05 15:28:33 +0900

    sqls

---
+++ 0131/insert_tickers.sql
@@ -0,0 +1,103 @@
+INSERT INTO TICKERS (STOCK_TICKER, STATUS, NAME, SECTOR, LISTING_DATE, DELISTING_DATE, EXCHANGE_CODE, TENDENCY,CREATE_TIME, UPDATE_TIME)
+SELECT 'AAPL', 1, 'Apple Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MSFT', 1, 'Microsoft Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'GOOGL', 1, 'Alphabet Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'GOOG', 1, 'Alphabet Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AMZN', 1, 'Amazon.com, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'TSLA', 1, 'Tesla, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'NVDA', 1, 'NVIDIA Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PEP', 1, 'PepsiCo, Inc.', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AVGO', 1, 'Broadcom Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'COST', 1, 'Costco Wholesale Corporation', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ADBE', 1, 'Adobe Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CSCO', 1, 'Cisco Systems, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CMCSA', 1, 'Comcast Corporation', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AMD', 1, 'Advanced Micro Devices, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'TXN', 1, 'Texas Instruments Incorporated', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AMGN', 1, 'Amgen Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SBUX', 1, 'Starbucks Corporation', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'INTC', 1, 'Intel Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'QCOM', 1, 'QUALCOMM Incorporated', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'HON', 1, 'Honeywell International Inc.', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'INTU', 1, 'Intuit Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AMAT', 1, 'Applied Materials, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PYPL', 1, 'PayPal Holdings, Inc.', 'Financial Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'BKNG', 1, 'Booking Holdings Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ADP', 1, 'Automatic Data Processing, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MDLZ', 1, 'Mondelez International, Inc.', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'NFLX', 1, 'Netflix, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CHTR', 1, 'Charter Communications, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PDD', 1, 'PDD Holdings Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ABNB', 1, 'Airbnb, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'REGN', 1, 'Regeneron Pharmaceuticals, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CSX', 1, 'CSX Corporation', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'VRTX', 1, 'Vertex Pharmaceuticals Incorporated', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'GILD', 1, 'Gilead Sciences, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'TMUS', 1, 'T-Mobile US, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'FISV', 1, 'None', 'NULL', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MU', 1, 'Micron Technology, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'LRCX', 1, 'Lam Research Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MRNA', 1, 'Moderna, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ISRG', 1, 'Intuitive Surgical, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ZM', 1, 'Zoom Communications Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ADSK', 1, 'Autodesk, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ROST', 1, 'Ross Stores, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MNST', 1, 'Monster Beverage Corporation', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'EA', 1, 'Electronic Arts Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MAR', 1, 'Marriott International, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'TEAM', 1, 'Atlassian Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PANW', 1, 'Palo Alto Networks, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AEP', 1, 'American Electric Power Company, Inc.', 'Utilities', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'KDP', 1, 'Keurig Dr Pepper Inc.', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'EXC', 1, 'Exelon Corporation', 'Utilities', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'XEL', 1, 'Xcel Energy Inc.', 'Utilities', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ORLY', 1, 'O'Reilly Automotive, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SNPS', 1, 'Synopsys, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MCHP', 1, 'Microchip Technology Incorporated', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AAL', 1, 'American Airlines Group Inc.', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'KLAC', 1, 'KLA Corporation', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ANSS', 1, 'ANSYS, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ODFL', 1, 'Old Dominion Freight Line, Inc.', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PAYX', 1, 'Paychex, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'AZN', 1, 'AstraZeneca PLC', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CDNS', 1, 'Cadence Design Systems, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CTAS', 1, 'Cintas Corporation', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'DXCM', 1, 'DexCom, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ALGN', 1, 'Align Technology, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'BKR', 1, 'Baker Hughes Company', 'Energy', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'VRSK', 1, 'Verisk Analytics, Inc.', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CPRT', 1, 'Copart, Inc.', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CHKP', 1, 'Check Point Software Technologies Ltd.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MRVL', 1, 'Marvell Technology, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'BIDU', 1, 'Baidu, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CERN', 1, 'None', 'NULL', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SPLK', 1, 'None', 'NULL', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'FAST', 1, 'Fastenal Company', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MELI', 1, 'MercadoLibre, Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CSGP', 1, 'CoStar Group, Inc.', 'Real Estate', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CRWD', 1, 'CrowdStrike Holdings, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'LULU', 1, 'Lululemon Athletica Inc.', 'Consumer Cyclical', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'WBA', 1, 'Walgreens Boots Alliance, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'VRSN', 1, 'VeriSign, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'KLXE', 1, 'KLX Energy Services Holdings, Inc.', 'Energy', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ROKU', 1, 'Roku, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SWKS', 1, 'Skyworks Solutions, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'IDXX', 1, 'IDEXX Laboratories, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'PCAR', 1, 'PACCAR Inc', 'Industrials', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'XLNX', 1, 'None', 'NULL', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'ALNY', 1, 'Alnylam Pharmaceuticals, Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CINF', 1, 'Cincinnati Financial Corporation', 'Financial Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'KHC', 1, 'The Kraft Heinz Company', 'Consumer Defensive', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'DDOG', 1, 'Datadog, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'DOCU', 1, 'DocuSign, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SEDG', 1, 'SolarEdge Technologies, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'SIRI', 1, 'Sirius XM Holdings Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'WDAY', 1, 'Workday, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'MTCH', 1, 'Match Group, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'OKTA', 1, 'Okta, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'NTES', 1, 'NetEase, Inc.', 'Communication Services', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'BMRN', 1, 'BioMarin Pharmaceutical Inc.', 'Healthcare', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'CEG', 1, 'Constellation Energy Corporation', 'Utilities', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+SELECT 'FTNT', 1, 'Fortinet, Inc.', 'Technology', NULL, NULL, 2, 0, SYSDATE, SYSDATE FROM DUAL UNION ALL
+;
+commit;
