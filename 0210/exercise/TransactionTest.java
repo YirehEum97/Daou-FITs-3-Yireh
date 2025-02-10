ca50fe830330ae91a70a616d91e4eabcdaaa478b
YirehEum97 <yreum97@daou.co.kr>
2025-02-10 16:45:26 +0900

    change TransactionTest.java

--- 0210/exercise/TransactionTest.java
+++ 0210/exercise/TransactionTest.java
@@ -1,42 +1,47 @@
 package exercise;
 
-import java.sql.*;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class TransactionTest {
     public static void main(String[] args) {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = null;
+            System.out.println("Driver loading Success");
 
+            Connection con = null;
             String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
             String ID = "C##yireh";
             String PW = "dlfp153";
-            con = DriverManager.getConnection(JDBC_URL, ID, PW);
-            System.out.print("Enter the keyword :");
+            con = DriverManager.getConnection(JDBC_URL,ID,PW);
+            // AutoCommit 끄기
+            // transaction 시작
+            con.setAutoCommit(false);
+            System.out.println("Database Connection Success");
 
-            Scanner s = new Scanner(System.in);
-            String keyword = s.nextLine();
-
-
-            String sql = "SELECT btitle, bauthor, bprice ";
-            sql += "FROM book WHERE btitle LIKE ?";
-//            System.out.println(sql);
+            String sql = "INSERT INTO MEMBERTBL values(?,?)";
             PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, "%" + keyword + "%");
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("btitle"));
+            pstmt.setString(1,"1");
+            pstmt.setString(2,"홍길동");
+
+            int result = pstmt.executeUpdate();
+
+            if (result == 1){
+                // result가 1인 경우 commit
+                con.commit();
+                System.out.println("insert Success");
+            } else {
+                // result가 1이 아닌 경우 오류가 있는 것이기 때문에 rollback
+                con.rollback();
             }
 
-            rs.close();
+            // 접속 종료
             pstmt.close();
             con.close();
-            System.out.println("DB Closed");
 
-        } catch (Exception e){
-            System.out.println("Error");
-            System.out.println(e);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
